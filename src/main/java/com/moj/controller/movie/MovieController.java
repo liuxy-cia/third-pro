2103c197dcc4e5cf68458a8fcc03bb8d1e947f51
LiuXinyu <1010714030@qq.com>
2020-04-03 14:41:32 +0800

    liu

---
+++ src/main/java/com/moj/controller/movie/MovieController.java
@@ -0,0 +1,15 @@
+package com.moj.controller.movie;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by 刘鑫宇 on 2020/4/2 19:43
+ */
+@Controller
+@RequestMapping("/movie")
+public class MovieController {
+
+
+
+}
