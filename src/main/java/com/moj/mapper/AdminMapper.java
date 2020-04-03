5839259e2049692831b01a270bc8c8dfb2319b9c
sss <1589034237@qq.com>
2020-04-03 15:19:14 +0800

    aaaq

--- src/main/java/com/moj/mapper/AdminMapper.java
+++ src/main/java/com/moj/mapper/AdminMapper.java
@@ -1,13 +1,26 @@
 package com.moj.mapper;
 
 import com.moj.entity.Admininformation;
+import com.moj.entity.Userinformation;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by yangshen  on 2020/3/23 0:59
  */
 @Mapper
+@Repository
 public interface AdminMapper {
     public Admininformation findByName(@Param("name") String name);
+    public List<Userinformation> selectUserInformation(@Param("start") String start, @Param("end") String end);
+    public List<Userinformation> selectUserInformationbyId(int id);
+    public List<Userinformation> selectUserInformationbyname(String name);
+    public List<Userinformation> selectUserInformationbyphone(String phone);
+    //查询条数
+   // public int selectCount();
+
+
 }
